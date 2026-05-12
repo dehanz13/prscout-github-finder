0f2a7457ba95d707b93a1c17e34ed95c468b4393
Danniel Hansel <hansel.danniel@gmail.com>
2026-05-11 21:04:17 -0500

    store the response of the call

--- src/App/GithubFinder/GithubFinder.tsx
+++ src/App/GithubFinder/GithubFinder.tsx
@@ -1,79 +1,96 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { kea, actions, listeners, reducers, useActions, useValues, path } from 'kea'
+
 import type { logicType } from './GithubFinderType'
 
 const API_URL = 'https://api.github.com'
 
 const logic = kea<logicType>([
-    path(['App', 'GithubFinder', 'GithubFinder']),
-    actions({
-        setUsername: (username) => ({ username }),
-        setRepositories: (repositories) => ({ repositories }),
-        setFetchError: (error) => ({ error }),
-    }),
-    reducers({
-        username: [
-            'keajs',
-            {
-                setUsername: (_, payload) => payload.username,
-            },
-        ],
-        repositories: [
-            [],
-            {
-                setUsername: () => [],
-                setRepositories: (_, { repositories }) => repositories,
-            },
-        ],
-        isLoading: [
-            false,
-            {
-                setUsername: () => true,
-                setRepositories: () => false,
-                setFetchError: () => false,
-            },
-        ],
-        error: [
-            null,
-            {
-                setUsername: () => null,
-                setFetchError: (_, { error }) => error,
-            },
-        ],
-    }),
+  path(['App', 'GithubFinder', 'GithubFinder']),
+  actions({
+    setUsername: (username) => ({ username }),
+    setRepositories: (repositories) => ({ repositories }),
+    setFetchError: (error) => ({ error }),
+  }),
+  reducers({
+    username: [
+      'keajs',
+      {
+        setUsername: (_, payload) => payload.username,
+      },
+    ],
+    repositories: [
+      [],
+      {
+        setUsername: () => [],
+        setRepositories: (_, { repositories }) => repositories,
+      },
+    ],
+    isLoading: [
+      false,
+      {
+        setUsername: () => true,
+        setRepositories: () => false,
+        setFetchError: () => false,
+      },
+    ],
+    error: [
+      null,
+      {
+        setUsername: () => null,
+        setFetchError: (_, { error }) => error,
+      },
+    ],
+  }),
 
-    listeners({
-        setUsername: async ({ username }) => breakpoint => {
-            // code to run when the username is set, e.g. fetch repos for that user
-            const url = `${API_URL}/users/${username}/repos?per_page=250`
+  listeners(({ actions }) => ({
+    setUsername: async ({ username }, breakpoint) => {
+      // code to run when the username is set, e.g. fetch repos for that user
+      const url = `${API_URL}/users/${username}/repos?per_page=250`
 
-            const response = await window.fetch(url)
-            const json = await response.json()
+      const response = await window.fetch(url)
+      const json = await response.json()
+      breakpoint()
 
-            if (response.status === 200) {
-                // we have repos in `json`
-
-            } else {
-                // there is an error with `json.message`
-
-            }
-        },
-    }),
+      if (response.status === 200) {
+        // we have repos in `json`
+        actions.setRepositories(json)
+      } else {
+        // there is an error with `json.message`
+        actions.setFetchError(json.message)
+      }
+    },
+  })),
 ])
 
 export function GithubFinder() {
-    const { username } = useValues(logic)
-    const { setUsername } = useActions(logic)
+  const { username, isLoading, repositories, error } = useValues(logic)
+  const { setUsername } = useActions(logic)
 
-    return (
-        <div className="example-github-scene">
-            <div style={{ marginBottom: 20 }}>
-                <h1>Search for a github user</h1>
-                <input value={username} type="text" onChange={(e) => setUsername(e.target.value)} />
+  return (
+    <div className="example-github-scene">
+      <div style={{ marginBottom: 20 }}>
+        <h1>Search for a github user</h1>
+        <input value={username} type="text" onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : repositories.length > 0 ? (
+        <div>
+          Found {repositories.length} repositories for user <strong>{username}</strong>!
+          {repositories.map((repo: any) => (
+            <div key={repo.id}>
+              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                {repo.full_name}
+              </a>
+              {' - '}
+              {repo.stargazers_count} stars, {repo.forks} forks.
             </div>
-            <div>
-                Repos will come here for user <strong>{username}</strong>
-            </div>
+          ))}
         </div>
-    )
+      ) : (
+        <div>{error ? `Error: ${error}` : 'No repositories found.'}</div>
+      )}
+    </div>
+  )
 }
